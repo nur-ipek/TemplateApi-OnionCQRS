387876f760fe8be30e2aef32cd7b1476b473f9be
nur.ipek.yilmaz@gmail.com <nur.ipek.yilmaz@gmail.com>
2024-05-01 16:19:24 +0300

    The project environments have been set up.

--- Presantation/TemplateApi.Api/Program.cs
+++ Presantation/TemplateApi.Api/Program.cs
@@ -7,6 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+var env = builder.Environment;
+
+builder.Configuration
+    .SetBasePath(env.ContentRootPath) //FileExplorer'da açtýðýmýzda çýkan path.
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
